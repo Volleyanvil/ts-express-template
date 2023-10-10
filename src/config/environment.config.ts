51cd015899b3e16751a8d5d88813c10e1c2dc263
Volleyanvil <visual.sixtyfour@gmail.com>
2023-10-10 20:26:58 +0300

    Added environment config unit tests. Updated unit tests.

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -1,31 +1,54 @@
-import 'dotenv/config';
+import { config as Dotenv } from 'dotenv';
 
 enum ENVIRONS {
   development = 'development',
   production = 'production',
-  testing = 'testing'
+  test = 'test'
+}
+
+interface EnvInterface {
+  ENV: string,
+  PORT: number,
+  HOST: string,
+  LOGS_PATH: string,
 }
 
 // TODO: Validate environmental variables
+export const EnvironmentWrapper = (): EnvInterface => {
+  let ENV: string = ENVIRONS.development;
+  if ( process.env.NODE_ENV ) {
+    ENV = ENVIRONS[process.env.NODE_ENV as ENVIRONS];
+  }
 
-let ENV: string = ENVIRONS.development;
-if (process.env.NODE_DEV) {
-  ENV = ENVIRONS[process.env.NODE_DEV as ENVIRONS];
-}
+  if ( ENV != ENVIRONS.test ) Dotenv(); // Skip in testing
 
-if ( process.env.PORT && (isNaN(parseInt(process.env.PORT)) || parseInt(process.env.PORT) > 65535)) {
-  console.log('PORT value is invalid. Use a valid TCP port number.');
-  process.exit(0);
-} 
-const PORT: number = parseInt(process.env.PORT) || 8000;
-
-const host_rx = /^((25[0-5]|2[0-4]\d|[01]?\d?\d)(\.|$)){4}$/;
-if ( process.env.PORT && !host_rx.test(process.env.HOST)) {
-    console.log('HOST value is invalid. Use a valid IPv4 address.');
+  if ( process.env.PORT && (isNaN(parseInt(process.env.PORT)) || parseInt(process.env.PORT) > 65535)) {
+    console.log('PORT value is invalid. Use a valid TCP port number.');
     process.exit(0);
   } 
-const HOST: string = process.env.HOST || '0.0.0.0';
+  const PORT: number = parseInt(process.env.PORT) || 8000;
 
-const LOGS_PATH: string = process.env.LOGS_PATH || 'logs';
+  const host_rx = /^((25[0-5]|2[0-4]\d|[01]?\d?\d)(\.|$)){4}$/;
+  if ( process.env.HOST && !( host_rx.test(process.env.HOST) || process.env.HOST === 'localhost' ) ) {
+      console.log('HOST value is invalid. Use a valid IPv4 address.');
+      process.exit(0);
+  }
+  const HOST: string = process.env.HOST || '0.0.0.0';
+
+  const LOGS_PATH: string = process.env.LOGS_PATH || 'logs';
+  return {
+    ENV: ENV,
+    PORT: PORT,
+    HOST: HOST,
+    LOGS_PATH: LOGS_PATH,
+  }
+};
+
+const env = EnvironmentWrapper();
+
+const ENV = env.ENV as string;
+const HOST = env.HOST as string;
+const PORT = env.PORT as number;
+const LOGS_PATH = env.LOGS_PATH as string;
 
 export { ENV, PORT, HOST, LOGS_PATH };
--- test/unit/index.spec.ts
+++ test/unit/index.spec.ts
@@ -1,29 +1,46 @@
-process.env.PORT = '3001';
 import express from 'express';
 import { Server } from 'net';
 
-console.log('process.version', process.version);
+console.log('process.version', process.version, ' ENV:', process.env.NODE_ENV);
 
 // NOTE: Unit testing an index file that only launches the server is largely unnecessary
 
 describe('Index', () => {
+  const ENV: NodeJS.ProcessEnv = process.env;
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = {...ENV};
+    process.env.NODE_ENV = ENV.NODE_ENV;
+    process.env.HOST = '0.0.0.0';
+    process.env.PORT = '8000';
+  });
+  afterAll(() => {
+    process.env = ENV;
+  });
+
   it('should work', async () => {
     const listen = jest.spyOn(Server.prototype, 'listen');
-
     jest.mock('@config/express.config', () => ({
       createServer: jest.fn().mockReturnValue(express()),
     }));
 
     await import('@server/index');
-
     expect(listen).toBeCalled();
 
     const server = listen.mock.results[0].value as Server;
-
     setImmediate(() => {
       server.close();
     });
-    
     listen.mockRestore();
   });
+
+  /*
+  it('should work', async () => {
+    process.env.PORT = 'abc';
+    const mockExit = jest.spyOn(process, 'exit').mockImplementation();
+
+    await import('@server/index');
+    expect(mockExit).toHaveBeenCalledWith(0);
+  });
+  */
 });
