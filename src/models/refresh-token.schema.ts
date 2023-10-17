78ff01248c16fbd08da069280ea543aeb542482e
Volleyanvil <52465201+Volleyanvil@users.noreply.github.com>
2023-10-17 15:12:37 +0300

    Updated refresh token schema, added token family fields for replay prevention

--- src/models/refresh-token.schema.ts
+++ src/models/refresh-token.schema.ts
@@ -3,16 +3,25 @@ import { Schema, model, Types } from 'mongoose';
 interface IRefreshToken {
   token: string,
   user: Types.ObjectId,
-  exp: Date,
+  token_expires: Date,
+  family_expires: Date,
+  family_root: string,
 }
 
 const RefreshTokenSchema = new Schema<IRefreshToken>({
   token: { type: String, required: true, unique: true },
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  exp: { type: Date, required: true },
+  token_expires: { type: Date, required: true },
+  family_expires: { type: Date, required: true },
+  family_root: { type: String, default: undefined }
 },
 { 
   timestamps: true 
+});
+
+// If token does not belong to a token family, set it as the root of a new family
+RefreshTokenSchema.pre('save', function() {
+  if (this.family_root === undefined) this.family_root = this.token;
 });
 
 export const RefreshToken = model<IRefreshToken>('RefreshToken', RefreshTokenSchema);
