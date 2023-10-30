b56a0cbded2c0ce330bafa0e0f4d92c22e469085
Volleyanvil <52465201+Volleyanvil@users.noreply.github.com>
2023-10-30 15:56:32 +0200

    Added main route for testing default passport jwt authentication

--- src/routes/main.route.ts
+++ src/routes/main.route.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import { MainController } from '@controllers/main.controller';
+import passport from 'passport';
 
 export class MainRouter {
     constructor() {
         this.router.route('/').get(this.controller.getMainPage);
         this.router.route('/health').get(this.controller.getHealth);
+        this.router.route('/auth-check').get(passport.authenticate('jwt', {session: false}), (req, res) => {
+            res.status(200).json(req.user);
+        });
     }
     router = Router();
     private controller = new MainController();
